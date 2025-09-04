cc098e9e3a2cc20c34c7787ea79e8cd13a8c3b9b
LucioWi <luciowiesek@gmail.com>
2025-09-03 22:28:50 -0300

    probando de nuevo

--- VisualStudio/Practica01/Practica01/Data/InvoiceRepository.cs
+++ VisualStudio/Practica01/Practica01/Data/InvoiceRepository.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Practica01.Domain;
 
 namespace Practica01.Data
 {
     public class InvoiceRepository : IInvoiceRepository
     {
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
 
+        public List<Invoice> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Invoice GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(Invoice invoice)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
